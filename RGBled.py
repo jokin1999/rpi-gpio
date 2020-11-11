7c4e6b1b7fc06ace1cda9912ea9518d7207fc0ba
dispnt <yuyuanjiajian@gmail.com>
2020-11-11 13:32:45 +0800

    Added comments

--- RGBled.py
+++ RGBled.py
@@ -8,9 +8,9 @@ import time
 import random
 
 # set rgb pin
-redPin   = 22
+redPin = 22
 greenPin = 18
-bluePin  = 16
+bluePin = 16
 
 print('running...')
 
@@ -20,26 +20,22 @@ GPIO.setup(redPin, GPIO.OUT)
 GPIO.setup(greenPin, GPIO.OUT)
 GPIO.setup(bluePin, GPIO.OUT)
 
+# Turn off all colors
 def offAll():
     GPIO.output(bluePin, GPIO.LOW)
     GPIO.output(greenPin, GPIO.LOW)
     GPIO.output(redPin, GPIO.LOW)
 
+
 def foo():
-    i=0
-    color_num = random.randint(1,3)
+    i = 0
+    color_num = random.randint(1, 2)  # Number of colors to light
     while i < int(color_num):
-        color = random.choice('rgb')
-        if color == 'r':
-            GPIO.output(redPin, GPIO.HIGH)
-        elif color == 'g':
-            GPIO.output(greenPin, GPIO.HIGH)
-        elif color == 'b':
-            GPIO.output(bluePin, GPIO.HIGH)
+        color = random.choice(['redPin', 'greenPin', 'bluePin'])
+        GPIO.output(globals()[color], GPIO.HIGH)  # Select a color to light
         i = i + 1
     time.sleep(0.2)
     offAll()
-
 
 
 while True:
